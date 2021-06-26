66deea3aa2c0e55c4947b6a5ee88de3e1708d153
CodeXMakerCompany <codexmaker12@gmail.com>
2021-06-26 13:54:07 -0500

    Getting status code and validating we send responses

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -2,4 +2,6 @@ const TodoModel = require('../model/todo.model');
 
 exports.createTodo = (req, res, next) => {
     TodoModel.create(req.body);
+
+    res.status(201).send();
 }; 
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -8,23 +8,35 @@ const newTodo =  require('../mock-data/new-todo.json');
 
 //Mocking the model of moongoose
 TodoModel.create = jest.fn();
+let req, res, next;
+
+ 
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+})
 
 describe('Todo controller tersting group', () => {
+
     it('should have a createTodo function', () => {
         expect(typeof TodoController.createTodo).toBe('function');
     });
 
     it('should call todoModel.create', () => {
-        let req, res, next;
-        req = httpMocks.createRequest();
-        res = httpMocks.createResponse();
-        next = null;
-
         req.body = newTodo;
 
         TodoController.createTodo(req, res, next);
         expect(TodoModel.create).toBeCalled();
 
         expect(TodoModel.create).toBeCalledWith(newTodo);
+    });
+
+    it('should return a response code', () => {
+        req.body = newTodo;
+
+        TodoController.createTodo(req, res, next);
+        expect(res.statusCode).toBe(201);
+        expect(res._isEndCalled()).toBeTruthy();
     });
 });
